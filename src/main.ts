cb3ddf32fbe259aa2819b0e2f89ff67159bc1bcc
Stevek <vegh@spsejecna.cz>
2026-01-17 16:57:19 +0100

    fixes

--- src/main.ts
+++ src/main.ts
@@ -74,7 +74,6 @@ async function startServer() {
     const server = net.createServer((socket) => {
         const remoteInfo = `${socket.remoteAddress}:${socket.remotePort}`;
         logger.info(`Připojen klient: ${remoteInfo}`);
-        socket.write("Zadejte příkaz>>\r\n");
         socket.setTimeout(CONFIG.CLIENT_IDLE_TIMEOUT);
         socket.on('timeout', () => {
             logger.warn(`Klient ${remoteInfo} odpojen pro neaktivitu.`);
